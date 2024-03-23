236d8bdd9c6fd7b79f86a83bdd7df8ca41a560b5
Bruno Sanzovo <gamebruno@gmail.com>
2024-03-23 10:51:25 -0300

    main

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const minutesDisplay = document.getElementById('minutes');
-const secondsDisplay = document.getElementById('seconds');
-const completedExercisesList = document.getElementById('completed-exercises-list');
-const startButton = document.getElementById('start');
-const pauseButton = document.getElementById('pause');
-const resetButton = document.getElementById('reset');
-const histButton = document.getElementById('history');
-
 let timer;
 let isRunning = false;
-let remainingTime = 25 * 60; // Inicialmente definido como 25 minutos
+let remainingTime = parseInt(localStorage.getItem('remainingTime')) || 25 * 60; // Inicialmente definido como 25 minutos
 let stretchInterval;
-let completedExercises = [];
+let completedExercises = JSON.parse(localStorage.getItem('completedExercises')) || [];
 
 const stretchExercises = [
     "Levante-se e estique os braços acima da cabeça por 10 segundos.",
@@ -18,98 +10,21 @@ const stretchExercises = [
     "Gire os ombros para trás em movimentos circulares por 20 segundos.",
     "Faça uma rotação no pescoço, movendo-o suavemente para a esquerda e para a direita por 10 segundos.",
     "Estique as pernas e toque os dedos dos pés, mantendo a posição por 15 segundos.",
-    
 ];
 
-// Recupera os dados do localStorage, se houver
-let storedRemainingTime = localStorage.getItem('remainingTime');
-let storedCompletedExercises = localStorage.getItem('completedExercises');
-
-// Verifica se há dados salvos e atribui-os às variáveis correspondentes
-remainingTime = storedRemainingTime ? parseInt(storedRemainingTime) : 25 * 60;
-completedExercises = storedCompletedExercises ? JSON.parse(storedCompletedExercises) : [];
-
-// Atualiza a exibição do temporizador e dos exercícios completados
-updateTimerDisplay();
-updateCompletedExercisesList();
-
 function updateTimerDisplay() {
+    const minutesDisplay = document.getElementById('minutes');
+    const secondsDisplay = document.getElementById('seconds');
     const minutes = Math.floor(remainingTime / 60);
     const seconds = remainingTime % 60;
     minutesDisplay.textContent = minutes < 10 ? `0${minutes}` : minutes;
     secondsDisplay.textContent = seconds < 10 ? `0${seconds}` : seconds;
 }
 
-function updateCompletedExercisesList() {
-    completedExercisesList.innerHTML = ''; // Limpa a lista antes de adicionar os exercícios
-
-    completedExercises.forEach(exercise => {
-        addToCompletedExercises(exercise);
-    });
-}
-
-function startTimer() {
-    if (!isRunning) {
-        timer = setInterval(updateTimer, 1000);
-        isRunning = true;
-        showExercise();
-    }
-}
-function startTimer2() {
-    if (!isRunning) {
-        isRunning = false;
-        showExercise2();
-    }
-}
-function pauseTimer() {
-    clearInterval(timer);
-    clearInterval(stretchInterval);
-    isRunning = false;
-
-    // Salva os dados no localStorage ao pausar o temporizador
-    localStorage.setItem('remainingTime', remainingTime);
-    localStorage.setItem('completedExercises', JSON.stringify(completedExercises));
-}
-
-function updateTimer() {
-    remainingTime--;
-    if (remainingTime >= 0) {
-        updateTimerDisplay();
-    } else {
-        clearInterval(timer);
-        completeExerciseButton.style.display = 'block';
-    }
-}
-
-function showExercise() {
-    let index = 0;
-    stretchInterval = setInterval(() => {
-        const currentExercise = stretchExercises[index];
-        addToCompletedExercises(currentExercise);
-        index = (index + 1) % stretchExercises.length;
-    }, 5 * 60 * 1000); // 5 minutos
-}
-function showExercise2(exercise) {
-    alert("1ªLevante-se e estique os braços acima da cabeça por 10 segundos\n2ªIncline o tronco para os lados, mantendo o alongamento por 15 segundos em cada lado.\n3ªGire os ombros para trás em movimentos circulares por 20 segundos.\n4ªFaça uma rotação no pescoço, movendo-o suavemente para a esquerda e para a direita por 10 segundos.\n5ªEstique as pernas e toque os dedos dos pés, mantendo a posição por 15 segundos.");
-}
-function completeExercise() {
-    const currentExercise = stretchExercises.find(exercise => exercise === exerciseDescription.textContent.replace('Exercício de Alongamento: ', ''));
-    if (currentExercise && !completedExercises.includes(currentExercise)) {
-        completedExercises.push(currentExercise);
-        addToCompletedExercises(currentExercise);
-    }
-    completeExerciseButton.style.display = 'none';
-
-    // Salva os dados no localStorage ao completar um exercício
-    localStorage.setItem('completedExercises', JSON.stringify(completedExercises));
-}
-
 function addToCompletedExercises(exercise) {
+    const completedExercisesList = document.getElementById('completed-exercises-list');
     const li = document.createElement('li');
-    const minutes = Math.floor(remainingTime / 60);
-    const seconds = remainingTime % 60;
-    const timeElapsed = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-    li.textContent = `${exercise} - Tempo: ${timeElapsed}`;
+    li.textContent = exercise;
     const img = document.createElement('img');
     img.src = 'assets/tarefaicon01.jpeg';
     img.alt = 'checkmark';
@@ -120,14 +35,43 @@ function addToCompletedExercises(exercise) {
     // Adiciona classe automaticamente para destacar exercícios concluídos após 20 segundos
     setTimeout(() => {
         li.classList.add('completed');
-    }, 60000);
+    }, 4 * 60 * 1000); // 4 minutos
 }
 
-startButton.addEventListener('click', startTimer);
-pauseButton.addEventListener('click', pauseTimer);
+function startTimer() {
+    if (!isRunning) {
+        timer = setInterval(() => {
+            remainingTime--;
+            if (remainingTime >= 0) {
+                updateTimerDisplay();
+            } else {
+                clearInterval(timer);
+                completeExerciseButton.style.display = 'block';
+            }
+        }, 1000);
+        isRunning = true;
+        showExercise();
+    }
+}
 
-resetButton.addEventListener('click', resetTimer);
-histButton.addEventListener('click', startTimer2);
+function pauseTimer() {
+    clearInterval(timer);
+    clearInterval(stretchInterval);
+    isRunning = false;
+
+    // Salva os dados no localStorage ao pausar o temporizador
+    localStorage.setItem('remainingTime', remainingTime);
+    localStorage.setItem('completedExercises', JSON.stringify(completedExercises));
+}
+
+function showExercise() {
+    let index = 0;
+    stretchInterval = setInterval(() => {
+        const currentExercise = stretchExercises[index];
+        addToCompletedExercises(currentExercise);
+        index = (index + 1) % stretchExercises.length;
+    }, 5 * 60 * 1000); // 5 minutos
+}
 
 function resetTimer() {
     clearInterval(timer); // Limpa o temporizador
@@ -138,5 +82,15 @@ function resetTimer() {
     localStorage.removeItem('remainingTime'); // Remove os dados do localStorage
     localStorage.removeItem('completedExercises'); // Remove os dados do localStorage
     updateTimerDisplay(); // Atualiza a exibição do temporizador
-    updateCompletedExercisesList(); // Atualiza a lista de exercícios concluídos
+    document.getElementById('completed-exercises-list').innerHTML = ''; // Limpa a lista de exercícios completados
 }
+
+document.getElementById('start').addEventListener('click', startTimer);
+document.getElementById('pause').addEventListener('click', pauseTimer);
+document.getElementById('reset').addEventListener('click', resetTimer);
+
+// Atualiza a exibição do temporizador e dos exercícios completados
+updateTimerDisplay();
+completedExercises.forEach(exercise => {
+    addToCompletedExercises(exercise);
+});
