0516f54c07c0f06b4f9def3972dab735175d68ae
Bruno Sanzovo <gamebruno@gmail.com>
2024-03-23 01:31:55 -0300

    main

---
+++ cadastro.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formCadastro = document.querySelector('.form-cadastro');
+
+    formCadastro.addEventListener('submit', function (event) {
+        event.preventDefault(); // Evita o envio padrão do formulário
+
+        // Obtenha os valores dos campos de entrada
+        const login = formCadastro.querySelector('input[type="text"]').value;
+        const senha = formCadastro.querySelector('input[type="password"]').value;
+
+        // Salve os dados no localStorage
+        localStorage.setItem('login', login);
+        localStorage.setItem('senha', senha);
+
+        // Redirecione para a página de login
+        window.location.href = 'principal.html';
+    });
+});
---
+++ principal.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formLogin = document.querySelector('.form-login');
+
+    formLogin.addEventListener('submit', function (event) {
+        event.preventDefault(); // Evita o envio padrão do formulário
+
+        // Obtenha os valores dos campos de entrada
+        const email = formLogin.querySelector('input[type="text"]').value;
+        const senha = formLogin.querySelector('input[type="password"]').value;
+
+        // Obtenha os dados salvos no localStorage
+        const savedEmail = localStorage.getItem('login');
+        const savedSenha = localStorage.getItem('senha');
+
+        // Verifique se os campos não estão vazios
+        if (email.trim() === '' || senha.trim() === '') {
+            alert('Por favor, preencha todos os campos.');
+            return;
+        }
+
+        // Verifique se os dados inseridos correspondem aos salvos
+        if (email === savedEmail && senha === savedSenha) {
+            // Redirecione para a página inicial (index.html) se o login for bem-sucedido
+            window.location.href = 'index.html';
+        } else {
+            // Exiba uma mensagem de erro caso o login falhe
+            alert('Login ou senha incorretos. Por favor, tente novamente.');
+        }
+    });
+
+    const entreAquiButton = document.querySelector('.button-login');
+
+    entreAquiButton.addEventListener('click', function (event) {
+        // Impede o redirecionamento padrão ao clicar no botão
+        event.preventDefault();
+
+        // Dispara o evento de submit no formulário de login
+        formLogin.dispatchEvent(new Event('submit'));
+    });
+});
